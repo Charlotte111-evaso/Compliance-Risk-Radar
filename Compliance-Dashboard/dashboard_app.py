0981f4d1fe37162b3ff46b1a3bfec98ff47cb1f3
Charlotte111-evaso <chazevason@gmail.com>
2025-07-01 04:17:25 +0200

    Update dashboard_app.py

--- Compliance-Dashboard/dashboard_app.py
+++ Compliance-Dashboard/dashboard_app.py
@@ -8,7 +8,7 @@ st.set_page_config(layout="wide")
 
 @st.cache_data
 def load_data():
-    return pd.read_csv("../Data/processed_users.csv")
+    return pd.read_csv("processed_users.csv")
 
 df = load_data()
 
